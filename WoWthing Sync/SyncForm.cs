22bb042a962933cf8a087008b9b3ee99f8fd78e6
Freddie <freddie@wafflemonster.org>
2015-10-01 18:13:02 +0930

    Clean up unused using statements

--- WoWthing Sync/SyncForm.cs
+++ WoWthing Sync/SyncForm.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using System.Timers;
 using System.Net.Http;
 using System.IO.Compression;
-using System.Threading.Tasks;
 using System.Linq;
 using System.Reflection;
 
